a32d4970dd14728e3b25b948f9353529ec378c82
zybrcwb <734307741@qq.com>
2017-09-10 20:54:22 +0800

    Initial commit

---
+++ src/main/java/com/zybr/windowshop/WindowshopApplication.java
@@ -0,0 +1,12 @@
+package com.zybr.windowshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowshopApplication.class, args);
+	}
+}
